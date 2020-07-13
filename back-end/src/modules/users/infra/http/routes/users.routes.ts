ebd8a797fd229d9dbfbbea04f6d1172fb7611efb
André Souza <als260602@msn.com>
2020-07-13 18:40:54 -0300

    refatoramento do backend no padrao solid

---
+++ back-end/src/modules/users/infra/http/routes/users.routes.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import multer from 'multer';
+
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import uploadConfig from '@config/upload';
+import UsersAvatarController from '../controllers/UsersAvatarController';
+import UsersController from '../controllers/UsersController';
+
+const usersRouter = Router();
+const upload = multer(uploadConfig);
+const usersController = new UsersController();
+const usersAvatarController = new UsersAvatarController();
+
+usersRouter.post('/', usersController.create);
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  usersAvatarController.update,
+);
+
+export default usersRouter;
--- back-end/src/modules/users/services/AuthenticateUserService.ts
+++ back-end/src/modules/users/services/AuthenticateUserService.ts
@@ -1,10 +1,11 @@
-import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
-import User from '../app/models/User';
-import authConfig from '../config/auth';
+import { injectable, inject } from 'tsyringe';
 
-import AppError from '../errors/AppError';
+import authConfig from '@config/auth';
+import AppError from '@shared/errors/AppError';
+import User from '../infra/typeorm/entities/User';
+import IUsersRepository from '../repositories/IUsersRepository';
 
 interface Request {
   email: string;
@@ -16,11 +17,15 @@ interface Response {
   token: string;
 }
 
+@injectable()
 export default class AuthenticateUserService {
-  public async execute({ email, password }: Request): Promise<Response> {
-    const usersRepository = getRepository(User);
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
 
-    const user = await usersRepository.findOne({ where: { email } });
+  public async execute({ email, password }: Request): Promise<Response> {
+    const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
       throw new AppError('Incorrect email/passowd conbinations', 401);
