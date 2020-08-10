7389799e49f82e0a76da39c1183e5981ed3e24ce
André Souza <als260602@msn.com>
2020-08-10 12:25:36 -0300

    Fix: acertando back-end

--- back-end/src/modules/appointments/services/CreateAppointmentService.ts
+++ back-end/src/modules/appointments/services/CreateAppointmentService.ts
@@ -39,6 +39,7 @@ class CreateAppointmentService {
 
     const findAppointmentsInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
+      provider_id,
     );
 
     if (findAppointmentsInSameDate) {
